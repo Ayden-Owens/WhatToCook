ab21fec633f9bb483fb70c3afa9ec1b25b7d73bd
Ayden-Owens <aydeno@email.sc.edu>
2023-11-17 14:09:40 -0500

    login success

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,41 +1,52 @@
-const express = require("express")
-const router = express.Router()
-const { Users } = require("../models")
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // Register a new user
 router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
     try {
-        const { username, email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
         const newUser = await Users.create({
-            username, 
-            email, 
-            password,
-        })
-        res.status(201).json({ user: newUser })
+            username: username,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({ user: newUser });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: 'Internal Server Error' })
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-})
+});
 
 // Login
 router.post('/login', async (req, res) => {
-    try{
-        const { username, password } = req.body
-        const foundUser = await Users.findOne({
-            where: { username, password },
-        })
+    const { username, password } = req.body;
 
-        if (foundUser) {
-            res.json({ message: 'Login successful', user: foundUser })
-        }
-        else {
-            res.status(401).json({ error: 'Invalid credentials' })
+    try {
+        const user = await Users.findOne({
+            where: { username: username },
+        });
+
+        if (user) {
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (passwordMatch) {
+                res.send({ message: 'Login successful', user });
+            } else {
+                res.status(401).json({ error: 'Wrong username/password combination' });
+            }
+        } else {
+            res.status(404).json({ message: "User doesn't exist" });
         }
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: 'Internal Server Error' })
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
